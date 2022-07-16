bab46fda0d48a27a907b81c97e3af36bb4c484d4
mahmoudessam16 <mahmoudemourad29@gmail.com>
2022-07-16 09:57:43 +0200

    adding Expanding Project

---
+++ main.js
@@ -0,0 +1,16 @@
+const panels = document.querySelectorAll(".panel");
+
+panels.forEach(panel => {
+    panel.addEventListener('click', function () {
+        removeClassActive();
+        panel.classList.add('active');
+    })
+})
+
+
+
+function removeClassActive() {
+    panels.forEach(panel => {
+        panel.classList.remove("active");
+    })
+}
